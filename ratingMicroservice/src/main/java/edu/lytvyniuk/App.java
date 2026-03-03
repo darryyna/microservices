d50f9f80dcd07cdfb83a2ab68f0cdc0fbb07e2c6
darynkka <dashalitviniuk20@gmail.com>
2026-03-03 13:25:39 +0200

    feat: docker usage impl

--- ratingMicroservice/src/main/java/edu/lytvyniuk/App.java
+++ ratingMicroservice/src/main/java/edu/lytvyniuk/App.java
@@ -8,12 +8,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Hello world!
  *
  */
+import java.util.TimeZone;
+
 @EnableDiscoveryClient
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
         SpringApplication.run(App.class, args);
     }
 }
