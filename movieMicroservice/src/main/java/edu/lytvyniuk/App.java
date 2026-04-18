85678f8b03f9444d1509411a15940d8887421eb5
darynkka <dashalitviniuk20@gmail.com>
2026-04-18 22:33:44 +0300

    add k8s

--- movieMicroservice/src/main/java/edu/lytvyniuk/App.java
+++ movieMicroservice/src/main/java/edu/lytvyniuk/App.java
@@ -1,9 +1,4 @@
 package edu.lytvyniuk;
-
-/**
- * Hello world!
- *
- */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
